542ffbad5b5f5562384c3e309c20da2aa10f4fd0
Yevhen Surfer <23skopets@gmail.com>
2024-04-29 10:34:59 +0200

    the game is working

--- Starter/obj/Debug/net6.0/Starter.AssemblyInfo.cs
+++ Starter/obj/Debug/net6.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1efaab859bf9e55af45067482cd8e84e13dae0c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73be12f75a6ffcf4619b6f19f4680ffb17c82139")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
